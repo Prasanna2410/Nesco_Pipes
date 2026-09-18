55ed5fe97bb42cb2d5403b0df61f049b05126dbe
Prasanna2410 <mahajanprasanna28@gmail.com>
2026-09-18 15:13:12 +0530

    Install Google Tag Manager site-wide

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -18,7 +18,31 @@ export const metadata: Metadata = {
 export default function RootLayout({ children }: Readonly<{ children: React.ReactNode }>) {
   return (
     <html lang="en" className={`${geist.variable} ${mono.variable} ${manrope.variable}`}>
-      <body>{children}</body>
+      <head>
+        {/* eslint-disable-next-line @next/next/next-script-for-ga -- Keep the supplied asynchronous GTM loader in the head without adding a package. */}
+        <script
+          id="google-tag-manager"
+          dangerouslySetInnerHTML={{
+            __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-KZM5TDVH');`,
+          }}
+        />
+      </head>
+      <body>
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-KZM5TDVH"
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+            title="Google Tag Manager"
+          />
+        </noscript>
+        {children}
+      </body>
     </html>
   );
 }
